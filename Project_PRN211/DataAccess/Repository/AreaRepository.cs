afc60cbe1df30e92206aefa55376a83abf8b26ef
hungvietle227 <leviethung220703@gmail.com>
2023-11-12 12:41:45 +0700

    Area

---
+++ Project_PRN211/DataAccess/Repository/AreaRepository.cs
@@ -0,0 +1,42 @@
+﻿using BussinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class AreaRepository : IAreaRepository
+    {
+        public void AddArea(Area area)
+        {
+            AreaDAO.Instance.AddArea(area);
+        }
+
+        public void DeleteArea(Area area)
+        {
+            AreaDAO.Instance.DeleteArea(area);
+        }
+
+        public List<Area> GetAllArea()
+        {
+            return AreaDAO.Instance.GetAllArea();
+        }
+
+        public Area GetAreaByID(string id)
+        {
+            return AreaDAO.Instance.GetAreaByID(id);
+        }
+
+        public Area GetAreaByName(string name)
+        {
+            return AreaDAO.Instance.GetAreaByName(name);
+        }
+
+        public void UpdateArea(Area area)
+        {
+            AreaDAO.Instance.UpdateArea(area);
+        }
+    }
+}
